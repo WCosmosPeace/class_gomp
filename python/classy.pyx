e14d7d83368cf5034ab8159240633e875f8fdbc5
Paulo Montero Camacho <pmontero@pcl.ac.cn>
2025-05-16 07:46:38 +0800

    Remove ppm in thermodynamics_init

--- python/classy.pyx
+++ python/classy.pyx
@@ -373,7 +373,7 @@ cdef class Class:
             self.ncp.add("background")
 
         if "thermodynamics" in level:
-            if thermodynamics_init(&(self.pr), &(self.ba), &(self.pm),
+            if thermodynamics_init(&(self.pr), &(self.ba),
                                    &(self.th)) == _FAILURE_:
                 self.struct_cleanup()
                 raise CosmoComputationError(self.th.error_message)
